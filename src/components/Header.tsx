9bf6c39ba4fc6a4c91a8ff7a0bbf87f477e0f1b6
a1c2r3t4 <alexandrecl001@gmail.com>
2022-09-20 19:04:12 -0400

    Simple Header component to begin with

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import { Box, Typography, AppBar, Toolbar } from '@mui/material';
+import { Account } from './Account'
+
+export const Header = () => {
+    return (
+        <AppBar >
+            <Toolbar sx={{
+                backgroundColor: 'primary.dark',
+                display: 'flex',
+                justifyContent: 'space-between',
+                padding: '1rem',
+                borderBottom: '8px solid #2A496A'
+            }}>
+                <Typography variant='h3'>PROJECTS TIME MANAGER</Typography>
+                <Account name='Alexandre Cloutier-Lemay' />
+            </Toolbar>
+        </AppBar>
+    )
+}
